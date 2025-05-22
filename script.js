5c755ee7107684cc07369d044a0dcc65582ed75e
Hamburger-coder <joesmast@gmail.com>
2025-05-22 17:59:15 -0400

    Built up boilerplate code

---
+++ script.js
@@ -0,0 +1,40 @@
+
+class LinkedList {
+    constructor() {
+
+    }
+    append(value) {
+
+    }
+    prepend(value) {
+
+    }
+    size() {
+
+    }
+    head() {
+
+    }
+    tail() {
+
+    }
+    at(index) {
+
+    }
+    pop() {
+
+    }
+    contains(value) {
+
+    }
+    find(value) {
+
+    }
+    toString() {
+        
+    }
+}
+
+class Node {
+
+}
